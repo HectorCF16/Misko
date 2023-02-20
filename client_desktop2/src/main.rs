dd36fda2bd32bfc61b412823fcf6d6524ba34015
HectorCF16 <66692284+HectorCF16@users.noreply.github.com>
2023-02-20 16:21:33 +0100

    Update main.rs

--- client_desktop2/src/main.rs
+++ client_desktop2/src/main.rs
@@ -3,23 +3,43 @@ use std::io::{Read, Write};
 use std::str::from_utf8;
 use std::{thread, time};
 
+struct DecomposedMessage {
+    position_x: i32,
+    position_y: i32,
+    press: bool,
+    release: bool,
+    right: bool
+}
+
 fn transform_i32_to_array_of_u8(number: i32) -> [u8; 4] {
+    //si el byte mes significatiu es major que 128 significa que el nombre es negatiu
+    println!("number: {}", number);
+    let is_negative = number < 0;
+    println!("negative {}", is_negative);
+    let positive_number = number * (-1_i32).pow(is_negative as u32);
+    println!("{}", positive_number);
     let mut result_array = [0 as u8; 4];
-    //suposant que el byte en el lloc 0 de l'array es el menys significatiu
-    for i in 0..4 {
-        print!("bit {}:", 3 - i);
-        result_array[3 - i] = ((number >> 8 * i) & 0xff) as u8;
+    //suposant que el byte en el lloc 0 de l'array es el mes significatiu i el que porta el bit negatiu
+    
+    for i in 0..3 {
+        print!("byte {}:", 3 - i);
+        result_array[3 - i] = ((positive_number >> 8 * i) & 0xff) as u8;
         println!("{}", result_array[3 - i]);
     }
+    print!("byte 0:");
+    let negative_bit = if is_negative {128 as u8} else {0 as u8};
+    result_array[0] = ((positive_number >> 8 * 3) & 0xff) as u8 + negative_bit;
+    println!("{}", result_array[0]);
+    
     println!("");
 
     result_array
 }
 
-fn message_from_coordinates(x: i32, y: i32) -> [u8; 8] {
-    let x_array = transform_i32_to_array_of_u8(x);
-    let y_array = transform_i32_to_array_of_u8(y);
-    let mut message_array = [0 as u8; 8];
+fn compose_message(decomposed_message: DecomposedMessage) -> [u8; 9] {
+    let x_array = transform_i32_to_array_of_u8(decomposed_message.position_x);
+    let y_array = transform_i32_to_array_of_u8(decomposed_message.position_y);
+    let mut message_array = [0 as u8; 9];
 
     for i in 0..4 {
         message_array[i] = x_array[i];
@@ -27,6 +47,9 @@ fn message_from_coordinates(x: i32, y: i32) -> [u8; 8] {
         
     }
 
+    message_array[8] = ((decomposed_message.press as u8) * 4) | ((decomposed_message.release as u8) * 2) | (decomposed_message.right as u8);
+
+    println!("9th bit: {}", message_array[8]);
     return message_array;
 }
 
@@ -35,34 +58,82 @@ fn main() {
     match TcpStream::connect("localhost:3333") {
         Ok(mut stream) => {
             println!("Successfully connected to server in port 3333");
-            let mut x = 0;
-            let mut y = 0;
-            loop{
+            let mut x = -1;
+            let mut y = -1;
+
+            /*loop{
                 let ten_millis = time::Duration::from_millis(200);
                 thread::sleep(ten_millis);
-                let msg = message_from_coordinates(x, y);
-                x+=1;
-                y+=1;
+                if x < 0 && x > -11 {
+                    x -=1;
+                }
+
+
+
+                let decomposed_message = DecomposedMessage{
+                    position_x: x,
+                    position_y: y,
+                    press: false,
+                    release: false,
+                    right: false,
+                };
+                let msg = compose_message(decomposed_message);
                 
                 stream.write(&msg).unwrap();
                 println!("Sent Hello, awaiting reply...");
-            }
-            
-            
-            /*let mut data = [0 as u8; 8]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == &msg {
-                        println!("Reply is ok!");
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
-                    }
-                },
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
-                }
             }*/
+            let decomposed_message = DecomposedMessage{
+                position_x: 40,
+                position_y: 0,
+                press: true,
+                release: false,
+                right: false,
+            };
+            let msg = compose_message(decomposed_message);
+            stream.write(&msg).unwrap();
+            thread::sleep(time::Duration::from_millis(400));
+
+            let decomposed_message = DecomposedMessage{
+                position_x: 40,
+                position_y: 0,
+                press: true,
+                release: false,
+                right: false,
+            };
+            let msg = compose_message(decomposed_message);
+            stream.write(&msg).unwrap();
+            thread::sleep(time::Duration::from_millis(400));
+
+
+            let decomposed_message = DecomposedMessage{
+                position_x: 0,
+                position_y: 40,
+                press: false,
+                release: false,
+                right: false,
+            };
+            let msg = compose_message(decomposed_message);
+            stream.write(&msg).unwrap();
+            thread::sleep(time::Duration::from_millis(400));
+            let decomposed_message = DecomposedMessage{
+                position_x: -40,
+                position_y: 0,
+                press: false,
+                release: false,
+                right: false,
+            };
+            let msg = compose_message(decomposed_message);
+            stream.write(&msg).unwrap();
+            thread::sleep(time::Duration::from_millis(400));
+            let decomposed_message = DecomposedMessage{
+                position_x: 0,
+                position_y: -40,
+                press: false,
+                release: true,
+                right: false,
+            };
+            let msg = compose_message(decomposed_message);
+            stream.write(&msg).unwrap();
         },
         Err(e) => {
             println!("Failed to connect: {}", e);
